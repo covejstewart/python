30a1720f1fa6cdb6df1ac0f38486bca6996f2ef6
Jeff Stewart <pyearesqrd@gmail.com>
2011-09-15 15:14:52 -0700

    updated readme

--- tdd_bowling/bowling.py
+++ tdd_bowling/bowling.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python
 
+class Bowling(object):
+   def __init__(self):
+      self._rolls = []
 
-def init():
-   return
+   def reset(self):
+      self._rolls[:] = []
+      return
 
-def roll(pin_count):
-   return
+   def roll(self,pin_count):
+      self._rolls.append(pin_count)
+      return
 
-def score():
-   return 0
+   def _is_spare(self, roll_index):
+      return self._rolls[roll_index] + self._rolls[roll_index + 1] == 10
+
+   def _is_strike(self, roll_index):
+      return (self._rolls[roll_index] == 10)
+
+   def _score_strike(self, roll_index):
+      return self._rolls[roll_index + 1] + self._rolls[roll_index + 2] + 10;
+
+   def _score_spare(self, roll_index):
+      return 10 + self._rolls[roll_index + 2]
+
+   def _score_normal(self, roll_index):
+      return self._rolls[roll_index] + self._rolls[roll_index+1]
+
+   def score(self):
+      score = 0
+      roll_index = 0;
+      for frame in range(10):
+         if self._is_strike(roll_index):
+            score = score + self._score_strike(roll_index)
+            roll_index = roll_index + 1
+         elif self._is_spare(roll_index):
+            score = score + self._score_spare(roll_index)
+            roll_index = roll_index + 2      
+         else:
+            score = score + self._score_normal(roll_index)
+            roll_index = roll_index + 2      
+      return score
+
+
--- tdd_bowling/bowling_test.py
+++ tdd_bowling/bowling_test.py
@@ -1,23 +1,67 @@
 #!/usr/bin/env python
 
-import bowling as game
+from bowling import Bowling
 
-def _test_score_gutter_game():
-   game.init()
-   for x in range(0,20):
-      game.roll(0);
-   assert game.score() is 0, 'score_gutter_game'
+game = Bowling()
+
+def _roll_many(number_of_rolls, pins_per_roll):
+   for x in range(0,number_of_rolls):
+      _roll_once(pins_per_roll)
    return
 
-def _test_score_gutter_game():
-   game.init()
-   for x in range(0,20):
-      game.roll(0);
-   assert game.score() is 0, 'score_gutter_game'
+def _roll_once(pins_knocked_down):
+   game.roll(pins_knocked_down)
    return
 
+def _reset_game():
+   game.reset()
+   return
+
+def _get_score():
+   return game.score();
+
+def _test_score_gutter_bowling():
+   _reset_game();
+   _roll_many(20, 0);
+   assert _get_score() == 0, 'score_gutter_bowling'
+   return
+
+def _test_score_all_ones():
+   _reset_game();
+   _roll_many(20, 1);
+   assert _get_score() == 20, 'score_gutter_bowling'
+   return
+
+def _test_score_single_spare():
+   _reset_game();
+   _roll_once(5)
+   _roll_once(5)
+   _roll_once(3)
+   _roll_many(17,0)
+   assert _get_score() == 16, 'score_single_spare'
+   return
+
+def _test_score_single_strike():
+   _reset_game();
+   _roll_once(10)
+   _roll_once(5)
+   _roll_many(17,0)
+   assert _get_score() == 20, 'score_single_strike'
+   return
+
+def _test_score_perfect_game():
+   _reset_game();
+   _roll_many(12, 10)
+   test = _get_score()
+   if test is 300:
+      print "Test passes"
+   assert _get_score() == 300, 'score_perfect_game'   
+   return
 
 if __name__ == "__main__":
-   _test_score_gutter_game()
+   _test_score_gutter_bowling()
    _test_score_all_ones()
+   _test_score_single_spare()
+   _test_score_single_strike()
+   _test_score_perfect_game()
 
